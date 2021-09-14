8fdd7012a03cdf7b84c24061bb37a3217d686719
jibonghoshbd <jibonghosh1510@gmail.comm>
2021-09-15 00:25:16 +0600

    image slider

---
+++ app.js
@@ -0,0 +1,24 @@
+const images = [
+    'images/img-1.jpg',
+    'images/img-2.jpg',
+    'images/img-3.jpg',
+    'images/img-4.jpg',
+    'images/img-5.jpg',
+    'images/img-6.jpg',
+    'images/img-7.jpg',
+    'images/img-8.jpg',
+    'images/img-9.jpg',
+    'images/img-10.jpg',
+    'images/img-11.jpg',
+]
+let imgIndex = 0;
+const imgElement = document.getElementById('image-slider');
+setInterval(() => {
+    if (imgIndex >= images.length) {
+        imgIndex = 0;
+    }
+    const imgUrl = images[imgIndex]
+    imgElement.setAttribute('src', imgUrl);
+    console.log(imgElement.setAttribute('src', imgUrl))
+    imgIndex++;
+}, 1000)
